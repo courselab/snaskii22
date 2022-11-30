2231e9642835331a5fb3d49e74b17a62967d1a82
Maíra Canal <mairacanal@riseup.net>
2022-11-29 22:20:53 -0300

    Use SPDX-License-Identifiers

--- src/utils.h
+++ src/utils.h
@@ -1,23 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0-only
 /**
-Copyright (c) 2022 - CCOS ICMC-USP
-
-This file is part of Snaskii22.
-Snaskii22 is based on Snaskii, available at
-https://github.com/courselab/snaskii.
-
-Snaskii22 is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-**/
+ * Copyright (c) 2022 - CCOS ICMC-USP
+ */
 
 #ifndef UTILS_H
 #define UTILS_H
